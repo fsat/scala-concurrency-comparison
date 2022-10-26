d21aa0f545c7bf0dff9a87ed1af9d365e78400b5
Felix Satyaputra <fsatyaputra@gmail.com>
2022-10-26 20:43:51 +1100

    Event interpreter - compiled w/ effect

--- fsm-zio/src/main/scala/example/compare/fsm/zio/nested/events/EventsAlgebra.scala
+++ fsm-zio/src/main/scala/example/compare/fsm/zio/nested/events/EventsAlgebra.scala
@@ -1,6 +1,6 @@
 package example.compare.fsm.zio.nested.events
 
-trait EventsAlgebra[F[_], FScoped[_], UFStream[_], Event] {
+trait EventsAlgebra[F[_], FScoped[_], FStream[_], Event] {
   def publish[T <: Event](event: T): F[Unit]
-  def subscribe(): FScoped[UFStream[Event]]
+  def subscribe(): FScoped[FStream[Event]]
 }
